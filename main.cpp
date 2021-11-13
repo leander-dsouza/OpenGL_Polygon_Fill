0a4251b60362471e78e5c6d76766cc636cba6fb4
Leander Stephen D'Souza <leanderdsouza1234@gmail.com>
2021-11-13 12:29:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,59 @@
 #include<GL/glut.h>
 
+// Polygonal declaration
+
+// Irregular Quadrilateral
+// const int num_of_sides = 4;
+// int coords[num_of_sides][2] = { \
+//                                 {90, 300}, \
+//                                 {420, 270}, \
+//                                 {380, 10}, \
+//                                 {50, 100}, \
+//                             };
+
+
+// Regular Pentagon
+// const int num_of_sides = 5;
+// int coords[num_of_sides][2] = { \
+//                                 {440, 188}, \
+//                                 {368, 412}, \
+//                                 {132, 412}, \
+//                                 {60, 188}, \
+//                                 {250, 50}, \
+//                             };
+
+// Regular Decagon
+const int num_of_sides = 10;
+int coords[num_of_sides][2] = { \
+                                {312, 60}, \
+                                {188, 60}, \
+                                {88, 132}, \
+                                {50, 250}, \
+                                {88, 368}, \
+                                {188, 440}, \
+                                {312, 440}, \
+                                {412, 368}, \
+                                {450, 250}, \
+                                {412, 132}, \
+                            };
+
+
+// Concave Pentagon - FAIL CASE
+// const int num_of_sides = 5;
+// int coords[num_of_sides][2] = { \
+//                                 {300, 60}, \
+//                                 {100, 60}, \
+//                                 {50, 300}, \
+//                                 {200, 190}, \
+//                                 {400, 300}, \
+//                             };
+
+
 
 void Intersection(float x1,float y1,float x2,float y2,int *LE,int *RE)
 {
     float x,M;
-    int t,y;
+    int t,y,flag = 0;
     
     // Swapping to ensure we start at the lower ordinate
     if(y1>y2)
@@ -29,6 +78,7 @@ void Intersection(float x1,float y1,float x2,float y2,int *LE,int *RE)
 
     for(y=y1; y<=y2; y++)
     {
+        
         // Check if point is inside the window
         if(x<(float)LE[y])
             LE[y]=(int)x;
@@ -41,36 +91,11 @@ void Intersection(float x1,float y1,float x2,float y2,int *LE,int *RE)
 }
 
 
+
 void Draw()
 {
-    int x,y,i;
+    int x,y,i,flag=0;
     int LE[500],RE[500]; // Scanline coordinates
-
-    // Irregular quadrilateral
-    // GLint P1[2] = {90, 300}, \
-    //     P2[2] = {420, 270}, \
-    //     P3[2] = {380, 10}, \
-    //     P4[2] = {50, 100};
-
-    // Regular Pentagon
-    // GLint P1[2] = {440, 188}, \
-    //     P2[2] = {368, 412}, \
-    //     P3[2] = {132, 412}, \
-    //     P4[2] = {60, 188}, \
-    //     P5[2] = {250, 50};
-
-    // Regular Decagon
-    GLint P1[2] = {312, 60}, \
-        P2[2] = {188, 60}, \
-        P3[2] = {88, 132}, \
-        P4[2] = {50, 250}, \
-        P5[2] = {88, 368}, \
-        P6[2] = {188, 440}, \
-        P7[2] = {312, 440}, \
-        P8[2] = {412, 368}, \
-        P9[2] = {450, 250}, \
-        P10[2] = {412, 132};
-
 
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -83,30 +108,20 @@ void Draw()
     // Draw the Edges
     glColor3f(1.0,0.0,0.0); // Red
     glBegin(GL_LINE_LOOP);
-    glVertex2iv(P1);
-    glVertex2iv(P2);
-    glVertex2iv(P3);
-    glVertex2iv(P4);
-    glVertex2iv(P5);
-    glVertex2iv(P6);
-    glVertex2iv(P7);
-    glVertex2iv(P8);
-    glVertex2iv(P9);
-    glVertex2iv(P10);
+
+    for (int i=0; i<num_of_sides; i++)
+    {
+        glVertex2iv(coords[i]);
+    }
     glEnd();
 
-
-    // Compute the intersections of all the edges
-    Intersection(P1[0],P1[1],P2[0],P2[1],LE,RE);
-    Intersection(P2[0],P2[1],P3[0],P3[1],LE,RE);
-    Intersection(P3[0],P3[1],P4[0],P4[1],LE,RE);
-    Intersection(P4[0],P4[1],P5[0],P5[1],LE,RE);
-    Intersection(P5[0],P5[1],P6[0],P6[1],LE,RE);
-    Intersection(P6[0],P6[1],P7[0],P7[1],LE,RE);
-    Intersection(P7[0],P7[1],P8[0],P8[1],LE,RE);
-    Intersection(P8[0],P8[1],P9[0],P9[1],LE,RE);
-    Intersection(P9[0],P9[1],P10[0],P10[1],LE,RE);
-    Intersection(P10[0],P10[1],P1[0],P1[1],LE,RE);
+    for (int i=0; i<num_of_sides; i++)
+    {
+        // Compute the intersections of all the edges
+        // modulus to ensure roundtheabout pairs are taken
+        Intersection(coords[i][0],coords[i][1],\
+            coords[(i+1)%num_of_sides][0],coords[(i+1)%num_of_sides][1],LE,RE); 
+    }
 
 
     // Draw the Fill
